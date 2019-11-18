55582e509be9d0cffa8940ccdf6036a3f94cb5c9
sojman007 <sojman007@yahoo.com>
2019-11-18 23:56:13 +0100

    init commit

---
+++ Data/ContosoPetsContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ContosoPets.Api.Models;
+namespace ContosoPets.Api.Data
+{
+    public class ContosoPetsContext:DbContext
+    {
+        public ContosoPetsContext(DbContextOptions<ContosoPetsContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products {get;set;}
+    }
+
+
+}
---
+++ Data/SeedData.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoPets.Api.Models;
+
+namespace ContosoPets.Api.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(ContosoPetsContext context)
+        {
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Squeaky Bone",
+                        Price = 20.99m
+                    },
+                    new Product
+                    {
+                        Name = "Knotted Rope",
+                        Price = 12.99m
+                    },
+                    new Product
+                    {
+                        Name="Tecno Phone",
+                        Price = 12.12m
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
